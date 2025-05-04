9d9520c8564a7d55af6c61f2ab400f33f70af1c7
opekku2001 <opekku2001@gmail,com>
2025-05-04 23:40:46 +0900

    chore: add xcodegen project spec and initial SwiftUI app

---
+++ Sources/BokujoDashCowcomeApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct BokujoDashCowcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, Cowcome!")
+                .padding()
+        }
+    }
+}
